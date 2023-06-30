e56206cdf134d293edb622622c4b6ab5edadcaa4
jose rodrigues <jose.cordova@icomp.ufam.edu.br>
2023-06-29 21:14:17 -0400

    uploading produto's  create, read, update

--- backend/src/resources/produto/produto.controller.ts
+++ backend/src/resources/produto/produto.controller.ts
@@ -1,9 +1,52 @@
-import {Response,Response} from "express"
+import { Request, Response } from 'express';
+import {
+  getAllProdutos,
+  createProduto,
+  getProduto,
+  updateProduto,
+} from './produto.services';
+import { CreateProdutoDto } from './produto.types';
 
-const index = async(req: Request, res: Response) => {};
-const create = async(req: Request, res: Response) => {};
-const read= async(req: Request, res: Response) => {};
-const update = async(req: Request, res: Response) => {};
-const remove = async(req: Request, res: Response) => {};
+const index = async (req: Request, res: Response) => {
+  try {
+    const produtos = await getAllProdutos();
+    res.status(200).json(produtos);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+const create = async (req: Request, res: Response) => {
+  const produto: CreateProdutoDto = req.body;
+  try {
+    const newProduto = await createProduto(produto);
+    res.status(201).json(newProduto);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+const read = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const produto = await getProduto(id);
+    if (produto === null)
+      return res.status(400).json({ msg: 'Produto não existe' });
+    else res.status(200).json(produto);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+const update = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const produto = req.body;
+  try {
+    const result = await updateProduto(id, produto);
+    if (produto === null)
+      return res.status(400).json({ msg: 'Produto não existe' });
+    else res.status(200).json({ msg: 'Produto atualizado' });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+const remove = async (req: Request, res: Response) => {};
 
-export default {index, create, read, update, remove};
+export default { index, create, read, update, remove };
--- backend/src/router/v1Router.ts
+++ backend/src/router/v1Router.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import pingRouter from '../resources/ping/ping.router';
-import produtoController from './resources/produto/produto.router'
+import produtoRouter from '../resources/produto/produto.router';
 
 const router = Router();
 
 router.use('/ping', pingRouter);
-router.use('/prpduto', produtoRouter);
+router.use('/produto', produtoRouter);
 
 export default router;
